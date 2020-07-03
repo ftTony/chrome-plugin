a947ef7d06b39bf8b04f2677d0f0a309cef7d20f
tony <172993974@qq.com>
2020-07-03 17:44:11 +0800

    修改配置

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,36 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript({
-      code: 'document.body.style.backgroundColor="red"'
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.management.getSelf(function (res) {
+      chrome.tabs.create({ url: 'chrome-extension://' + res.id + '/src/index.html#normal' });
     });
   });
+  
+  
+  chrome.webRequest.onHeadersReceived.addListener(details => {
+    const responseHeaders = details.responseHeaders.map(header => {
+      const isCSPHeader = /content-security-policy/i.test(header.name);
+      const isFrameHeader = /x-frame-options/i.test(header.name);
+      if (isCSPHeader) {
+        let csp = header.value
+        // csp = csp.replace('script-src', `script-src ${hosts}`)
+        // csp = csp.replace('style-src', `style-src ${hosts}`)
+        // csp = csp.replace('frame-src', `frame-src ${iframeHosts}`)
+        // csp = csp.replace('child-src', `child-src ${hosts}`)
+        csp = csp.replace(/frame-ancestors (.*?);/ig, "");
+        // header.value = csp;
+        header.value = csp + 'sandbox;';
+      } else if (isFrameHeader) {
+        header.value = 'ALLOWALL';
+      }
+      return header
+    });
+    return { responseHeaders };
+  },
+    {
+      urls: ['<all_urls>', '*://*/*'],
+      types: ['sub_frame']
+    },
+    [
+      'blocking',
+      'responseHeaders'
+    ]
+  )
